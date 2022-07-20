a4c1330e3b4369cbd733291293bc2fb7a231eb2b
Kalash Thakur <83134042+KalashThakur@users.noreply.github.com>
2022-07-20 11:16:16 +0530

    added logo

--- src/Components/HeaderCom/Header.jsx
+++ src/Components/HeaderCom/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      <h1>Header</h1>
+      <img src="https://cdn.shopify.com/s/files/1/0283/0185/2747/files/bluemercury-logo_1216x.png?v=1648743182" alt="bluemercury-logo" />
     </div>
   )
 }
