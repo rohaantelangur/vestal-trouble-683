e67afb2daea31429667ed549541754817948ea2b
prabhuRV <katravthprabhu151@gmail.com>
2022-07-21 19:40:19 +0530

    cart fuctionalty

---
+++ src/Components/CartCom/CartShowList.jsx
@@ -0,0 +1,161 @@
+import {
+  Box,
+  Button,
+  Heading,
+  Image,
+  SlideFade,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+
+import { useDispatch } from "react-redux";
+import { DeleteIcon } from "@chakra-ui/icons";
+import { deleteitem, getCart } from "../../Redux/CartReducer/action";
+export const CartShowList = (props) => {
+  const { price, type, title, quantity, vendor, id } = props;
+  const { src } = props.images[0];
+  const [count, setCount] = useState(quantity);
+  const [abovethreeOrder, setAbovethreeOrder] = useState(false);
+  const dispatch = useDispatch();
+
+  const handdleUpdate = async (count) => {
+    await axios
+      .patch(`http://localhost:8080/addtocart/${id}`, {
+        quantity: count,
+        priceMax: count * price,
+      })
+      .then(() => getCart(dispatch));
+  };
+
+  const handdleIncrement = (value) => {
+    if (value == 1 && count < 3) {
+      setCount(count + value);
+    } else if (value === -1 && count > 1) {
+      setCount(count + value);
+    }
+  };
+  useEffect(() => {
+    if (count == 3) {
+      setAbovethreeOrder(true);
+    } else {
+      setAbovethreeOrder(false);
+    }
+  }, [count]);
+  useEffect(() => {
+    handdleUpdate(count);
+  }, [count]);
+  // console.log(abovethreeOrder)
+  const handlleDelete = async () => {
+   
+    deleteitem(dispatch, id).then(() => {
+      getCart(dispatch);
+    });
+  };
+
+  return (
+    <Box>
+      <Box
+        display={"flex"}
+        flexWrap={"wrap"}
+        gap="20px"
+        justifyContent={"space-between"}
+      >
+        <Stack direction={"row"}>
+          <Box w={"120px"} h="150px">
+            <Image src={src} />
+          </Box>
+          <Box width={"180px"} overflow={"hidden"} whiteSpace={"nowrap"}>
+            <Text>{title}</Text>
+            <Text color={"gray"}>{vendor}</Text>
+            <Text color={"#dcdcdc"}>{type}</Text>
+            <Box>{abovethreeOrder ? <Text>Limit: 3 Per Order</Text> : ""}</Box>
+          </Box>
+        </Stack>
+        <Box>
+          <Box display={"flex"} justifyContent="space-between">
+            <Text color={"gray"}>
+              ₹.
+              {price.toLocaleString("hi-IN")}.00
+            </Text>
+          </Box>
+        </Box>
+        <Box
+          display={"block"}
+          h="30px"
+          mt="10px"
+          w={"100px"}
+          justifyContent="space-around"
+          textAlign="center"
+        >
+          <Box
+            display={"flex"}
+            h="30px"
+            mt="10px"
+            w={"100px"}
+            justifyContent="space-around"
+            textAlign="center"
+          >
+            <Button
+              bgColor="#f8f8f8"
+              fontSize={"20px"}
+              borderRadius="0px"
+              border="1px black solid"
+              w={"40px"}
+              h="30px"
+              _hover={{
+                cursor: "pointer",
+              }}
+              onClick={() => handdleIncrement(-1)}
+            >
+              -
+            </Button>
+            <Box
+              bgColor="#ffffff"
+              border="1px black solid"
+              h="30px"
+              w="40px"
+              m={"auto"}
+            >
+              {quantity}
+            </Box>
+            <Button
+              borderRadius="0px"
+              bgColor="#f8f8f8"
+              fontSize={"20px"}
+              border="1px black solid"
+              w={"40px"}
+              h="30px"
+              onClick={() => handdleIncrement(1)}
+              _hover={{
+                cursor: "pointer",
+              }}
+            >
+              +
+            </Button>
+          </Box>
+
+          <Box>
+            <Text
+              _hover={{
+                cursor: "pointer",
+              }}
+              onClick={handlleDelete}
+            >
+              Remove
+            </Text>
+          </Box>
+        </Box>
+        <Box display={"flex"} justifyContent="space-between">
+          <Text color={"gray"}>
+            ₹.
+            {(quantity * price).toLocaleString("hi-IN")}.00
+          </Text>
+        </Box>
+      </Box>
+      <Box borderBottom={"1px solid #dcdcdc"} mt="3" mb={"3"} ></Box>
+    </Box>
+  );
+};
--- src/Components/CartCom/MainCartBag.jsx
+++ src/Components/CartCom/MainCartBag.jsx
@@ -1,11 +1,30 @@
 import { WarningIcon } from "@chakra-ui/icons";
-import { Box, Heading, HStack, Text } from "@chakra-ui/react";
+import { Box, Checkbox, Heading, HStack, Stack, Text } from "@chakra-ui/react";
 import React from "react";
-
+import {  useSelector } from "react-redux";
+import { CartShowList } from "./CartShowList";
+import { SideCartShow } from "./SideCartShow";
 export const MainCartBag = () => {
+  const AddtoCart = useSelector((state) => state.Cartreducer.AddtoCart);
+
+  const uniqueIds = [];
+
+const unique = AddtoCart.filter(element => {
+const isDuplicate = uniqueIds.includes(element.id);
+
+if (!isDuplicate) {
+  uniqueIds.push(element.id);
+
+  return true;
+}
+
+
+return false;
+});
+let totalPrice=0
   return (
-    <Box>
-      <Box p={10} ml={"8%"} fontFamily={`"Montserrat Regular",sans-serif`}>
+    <Box width={"80%"} m="auto">
+      <Box pt="10" pb="10" fontFamily={`"Montserrat Regular",sans-serif`}>
         <Heading
           fontWeight={500}
           fontStyle="normal"
@@ -19,10 +38,10 @@ export const MainCartBag = () => {
           YOUR BAG
         </Heading>
       </Box>
-      <HStack spacing={"10px"} justifyContent={"flex-end"} h="50px" mr={"6%"}  >
+      <HStack spacing={"10px"} justifyContent={"flex-end"} h="50px" mr={"4%"}>
         <Box
           gap={"10px"}
-          width="400px"
+          width="500px"
           fontSize={"20px"}
           display="flex"
           fontWeight={400}
@@ -30,16 +49,99 @@ export const MainCartBag = () => {
           bgColor={"#f4fafc"}
           h="50px"
           fontFamily={`"Montserrat Regular",sans-serif`}
-         pl={10}
+          pl={10}
         >
           <Box mt="2">
             <WarningIcon color="red.500" w={5} h={5} />
           </Box>
           <Text mt="3" fontSize={"14px"} verticalAlign={"baseline"}>
-            Your Order Qualifies for Free Ground Shipping!
+            Sign up or Sign in to BlueRewards for Free Ground Shipping
           </Text>
         </Box>
       </HStack>
+
+      <Box mt="5" fontFamily={`"Montserrat Regular",sans-serif`}>
+        <Heading
+          letterSpacing={"1px"}
+          fontSize="20px"
+          lineHeight={"26px"}
+          color={"#12284c"}
+          fontWeight={400}
+          as="h2"
+          size="xl"
+        >
+          Your cart ({AddtoCart.length} items)
+        </Heading>
+      </Box>
+      <Box mt={6}>
+        <Stack
+          gap={"15px"}
+          direction="row"
+          fontSize="25px"
+          color={"#12284c"}
+          fontWeight={700}
+          as="h2"
+          size="xl"
+        >
+          <Checkbox size="lg">IS THIS ORDER A GIFT?</Checkbox>
+        </Stack>
+        <Text
+          mt="5"
+          fontFamily={`"Montserrat Regular",sans-serif`}
+          fontSize={"14px"}
+          verticalAlign={"baseline"}
+          lineHeight={"19px"}
+          fontWeight={500}
+          color={"gray"}
+          ml="10px"
+        >
+          The price of your order will be hidden on the gift receipt.
+        </Text>
+      </Box>
+      <Box mt="10">
+        <HStack justifyContent={"space-between"} borderBottom={"1px solid #dcdcdc"} h="20" mb={"3"}>
+          <Box>
+            <Heading
+              letterSpacing={"1px"}
+              fontSize="20px"
+              lineHeight={"26px"}
+              color={"#12284c"}
+              fontWeight={400}
+              as="h2"
+              size="xl"
+            >
+              Ready to Ship
+            </Heading>
+          </Box>
+          <Stack direction={"row"} justifyContent={"space-around"} gap={220} fontFamily={`"Montserrat Regular",sans-serif`}
+          fontSize={"16px"}
+          verticalAlign={"baseline"}
+          lineHeight={"29px"}
+          fontWeight={400}
+          color={"gray"}>
+            <Box>
+              <Text>Price</Text>
+            </Box>
+            <Box>
+              <Text>Quantity</Text>
+            </Box>
+            <Box>
+              <Text>Total</Text>
+            </Box>
+          </Stack>
+        </HStack>
+          
+          <Box mt="2">
+              {
+                  unique.map((item)=>
+                  {
+                    totalPrice+=item.priceMax
+                      return  <CartShowList key={item.id} {...item} />
+                  })
+              }
+         
+          </Box>
+      </Box>
     </Box>
   );
 };
