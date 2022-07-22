6dbfdc71050d637b12490540294f22595a3676e9
Kalash Thakur <83134042+KalashThakur@users.noreply.github.com>
2022-07-22 19:36:24 +0530

    singleProduct

--- src/Components/SingleProCom/SingleProduct.jsx
+++ src/Components/SingleProCom/SingleProduct.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import style from "./SingleProduct.module.css";
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 export const SingleProduct = () => {
 
   const {id} = useParams();
-  // const product = useSelector((state) => state.)
+  const product = useSelector((state) => state.Appreducer.products);
+  const [currentProduct, setProduct] = useState([]);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if(id) {
+      const temp = product?.find((product) => product.id === Number(id))
+      temp && setProduct(temp) 
+    }
+  }, [product, id])
 
   return (
     <div className={style.mainDiv}>
-        <div></div>
+        <div>
+          <img src={currentProduct.image} alt="" />
+        </div>
+
         <div></div>
     </div>
   )
