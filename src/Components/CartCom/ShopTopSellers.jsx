23d57a5769ade8c16c40edd59da20c6427a01369
prabhuRV <katravthprabhu151@gmail.com>
2022-07-22 13:06:06 +0530

    cart last commit

---
+++ src/Components/CartCom/ShopTopSellers.jsx
@@ -0,0 +1,63 @@
+import { Box, Heading, Image, Stack, Text } from '@chakra-ui/react'
+import React from 'react'
+import {shopTopSellers} from "./sliderdata"
+// Import Swiper styles
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./styles.css";
+import { Pagination, Navigation } from "swiper";
+
+
+export const ShopTopSellers = () => {
+    
+  return (
+    <Box h="350px" mt="20" mb="10" fontFamily={`"Montserrat Regular",sans-serif`} color="#12284C">
+      <Stack ml={"30%"}>
+
+      <Heading size={"md"}letterSpacing="2px"  fontSize="24px" fontWeight={400} p="10">BEAUTY LOVERS LIKE YOU ALSO BOUGHT</Heading>
+      </Stack >
+
+       <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        slidesPerGroup={4}
+        loop={true}
+        loopFillGroupWithBlank={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiper"
+      >
+        {shopTopSellers.map((item)=>
+        {
+          return  <SwiperSlide>
+          <Stack fontSize={"14px"}>
+
+          <Image color={"#12284c"} h="50px"src={item.image} />
+          <Text>{item.brand}</Text>
+          <Text color={"gray"} >
+            {item.name}
+          </Text>
+          <Text color={"gray"}>
+            $. {item.price}
+          </Text>
+          </Stack>
+        </SwiperSlide>
+       
+        })
+
+        }
+       
+        
+      </Swiper>
+    </Box>
+  )
+}
+
--- src/Components/CartCom/SlidersPrivew.jsx
+++ src/Components/CartCom/SlidersPrivew.jsx
@@ -1,39 +1,61 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Heading, Image, Stack, Text } from '@chakra-ui/react'
 import React from 'react'
+import {sliderData} from "./sliderdata"
+// Import Swiper styles
 import { Swiper, SwiperSlide } from "swiper/react";
-import {slideone} from "./sliderdata.js/sliderData"
+
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-import "./style.css";
+import "./styles.css";
+import { Pagination, Navigation } from "swiper";
 
 
 export const SlidersPrivew = () => {
-    console.log(slideone)
+    
   return (
-    <Box>
-        <Swiper
-        slidesPerView={3}
+    <Box h="350px" mt="20" mb="10" fontFamily={`"Montserrat Regular",sans-serif`} color="#12284C">
+      <Stack ml={"40%"}>
+
+      <Heading size={"md"}letterSpacing="2px"  fontSize="24px" fontWeight={400} p="10">SIMILAR PRODUCTS</Heading>
+      </Stack >
+
+       <Swiper
+        slidesPerView={4}
         spaceBetween={30}
-        slidesPerGroup={3}
+        slidesPerGroup={4}
         loop={true}
         loopFillGroupWithBlank={true}
-        
-       
-   
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        {sliderData.map((item)=>
+        {
+          return  <SwiperSlide>
+          <Stack fontSize={"14px"}>
+
+          <Image color={"#12284c"} h="50px"src={item.image} />
+          <Text>{item.brand}</Text>
+          <Text color={"gray"} >
+            {item.name}
+          </Text>
+          <Text color={"gray"}>
+            $. {item.price}
+          </Text>
+          </Stack>
+        </SwiperSlide>
+       
+        })
+
+        }
+       
+        
       </Swiper>
     </Box>
   )
