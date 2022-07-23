138a6724d53a4ad6470c1dc5bf03c923940eb89c
Rohaan Telangur <94789421+rohaantelangur@users.noreply.github.com>
2022-07-23 12:59:31 +0530

    Image Issue Sol

--- src/Components/SingleProCom/SingleProduct.jsx
+++ src/Components/SingleProCom/SingleProduct.jsx
@@ -1,32 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import style from "./SingleProduct.module.css";
-import { useSelector, useDispatch } from 'react-redux';
-import { getProducts } from '../../Redux/AppReducer/action';
 import axios from 'axios';
+import { Box, Image, Img, Text } from '@chakra-ui/react';
 
 export const SingleProduct = () => {
 
   const { id } = useParams();
   const { category } = useParams();
-  // const product = useSelector((state) => state.Appreducer.products);
-  // console.log('product:', product)
-  const [currentProduct, setProduct] = useState({});
-  // const dispatch = useDispatch();
-  console.log(category,id,currentProduct.images[0].src);
+  const [currentProduct, setcurrentProduct] = useState({});
+  const [currentImage1, setcurrentImage1] = useState("");
+  const [currentImage2, setcurrentImage2] = useState("");
+  
 
-  useEffect(()=>{
-    axios.get(`http://localhost:8080/${category}/${id}`).then((res)=>{
-      setProduct(res.data)
+  const  getData = ()=>{
+    axios.get(`http://localhost:8080/${category}/${id}`)
+    .then((res)=>{
+      setcurrentProduct(res.data)
+      setcurrentImage1(res.data.images[0].src)
+      setcurrentImage2(res.data.images[1].src)
     }).catch((err)=>{
       console.log(err);
     })
+  }
 
-  },[])
+  useEffect(()=>{
+  getData()
+  console.log(currentProduct);
+  },[ ])
   return (
     <div className={style.mainDiv}>
         <div>
-          <img src={`https://www.${currentProduct.images[0].src}`} alt="" />
+          {currentProduct.length > 0 || 
+          <Box>
+          <Img
+        cursor={"pointer"}
+        src={currentImage1}
+        w="100%"
+        h="200px"
+        alt=""
+        srcSet=""
+        />
+        <Img
+        cursor={"pointer"}
+        src={currentImage2}
+        w="100%"
+        h="200px"
+        alt=""
+        srcSet=""
+        />
+
+          <Text>{currentProduct.title}</Text>
+          </Box>
+          }
         </div>
         <div></div>
     </div>
